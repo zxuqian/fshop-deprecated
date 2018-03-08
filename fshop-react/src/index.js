8fcd59a7919cfcc09207d93b8a6c15c3ccc50935
Xuqian <zxuqian@gmail.com>
2018-03-08 19:02:07 +0800

    -- Category insertion and query.

--- fshop-react/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {BrowserRouter as Router, Route, Link, Switch, withRouter} from 'react-router-dom'
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<Router><App /></Router>, document.getElementById('root'));
-registerServiceWorker();
